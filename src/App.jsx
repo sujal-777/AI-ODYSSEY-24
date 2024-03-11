2b83fbb74be9b02197b92b92c74ad592ee067dbf
Sujal <palsujal707@gmail.com>
2024-03-11 22:38:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Services from "./components/Services";
+// import useCountdown from "./components/useCountdown";
+// import useCountdown from "./components/useCountdown";
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        {/* <useCountdown /> */}
+        <Collaboration />
+        <Benefits />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
+      </div>
+
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
---
+++ src/hooks/useCountdown.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+
+export const EVENT_STARTING_DATETIME = "2024-04-05 10:00:00 AM";
+
+/**
+ * this function returns the days, hours, minutes, seconds left
+ * @param {number} currentCountdown number of milliseconds left
+ * @returns days, hours, minutes, seconds left
+ */
+export const getReturnValues = (currentCountdown = 0) => {
+    // calculate time left
+    const days = Math.floor(currentCountdown / (1000 * 60 * 60 * 24));
+
+    const hours = Math.floor(
+        (currentCountdown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+
+    const minutes = Math.floor(
+        (currentCountdown % (1000 * 60 * 60)) / (1000 * 60)
+    );
+
+    const seconds = Math.floor((currentCountdown % (1000 * 60)) / 1000);
+
+    // return the time left
+    return {
+        Days: days,
+        Hours: hours,
+        Mins: minutes,
+        Secs: seconds,
+    };
+};
+
+/**
+ * hook to get the time left for the event
+ * @returns days, hours, minutes, seconds left
+ */
+export const useCountdown = () => {
+    const currentDate = new Date().getTime();
+    const countdownDatetime = new Date(EVENT_STARTING_DATETIME).getTime();
+
+    // state to hold the time left
+    const [countDown, setCountDown] = useState(countdownDatetime - currentDate);
+
+    // update the time left every second
+    useEffect(() => {
+        // check if the event has started
+        const countInterval = setInterval(() => {
+            // update the time left
+            setCountDown(countdownDatetime - new Date().getTime());
+        }, 1000);
+
+        return () => clearInterval(countInterval);
+    }, [countdownDatetime]);
+
+    /**
+     * this hook calls the above utility function to get the days, hours, minutes, seconds left
+     * @returns days, hours, minutes, seconds left
+     */
+    return getReturnValues(countDown);
+};
